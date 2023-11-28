1ae8368d35869d647a8a830322c1f0e54acdc44a
keissaisono <keissa@hawaii.edu>
2023-11-27 19:29:21 -1000

    update

---
+++ public/login.js
@@ -0,0 +1,38 @@
+// Function to redirect to the invoice page
+function redirectToInvoice() {
+    // Perform login validation logic here
+
+    // For demonstration purposes, assume the login is successful
+    // You should replace the following line with your actual validation logic
+    let loginSuccessful = true;
+
+    if (loginSuccessful) {
+        // Redirect to the invoice page
+        window.location.href = './invoice.html';
+    }
+    
+    // Prevent the form from submitting (optional, depending on your needs)
+    return false;
+}
+
+// Function to redirect back to the store page
+function redirectToStore() {
+    window.location.href = './store.html';
+}
+
+ // Function to redirect to the invoice page
+ function redirectToInvoice() {
+    // Perform login validation logic here
+
+    // For demonstration purposes, assume the login is successful
+    // You should replace the following line with your actual validation logic
+    let loginSuccessful = true;
+
+    if (loginSuccessful) {
+        // Redirect to the invoice page
+        window.location.href = './invoice.html';
+    }
+    
+    // Prevent the form from submitting (optional, depending on your needs)
+    return false;
+}
